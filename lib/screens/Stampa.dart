29285c0342db0d684fd9d76036508d7f3e6c0b47
nikzanda <nikzanda@hotmail.it>
2020-12-30 18:13:28 +0100

    pagina stampa: formato e copie

---
+++ lib/screens/Stampa.dart
@@ -0,0 +1,85 @@
+// import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Stampa extends StatefulWidget {
+  Stampa({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _StampaState createState() => _StampaState();
+}
+
+enum Formato { radio_12_14, radio_15, radio_altro }
+
+class _StampaState extends State<Stampa> {
+  Formato formato = Formato.radio_12_14;
+  int copie = 1;
+
+  void onRadioFormato(newValue) {
+    setState(() => formato = newValue);
+  } //onRadioFormato
+
+  void onSliderCopie(newValue) {
+    setState(() => copie = newValue.round());
+  } //onSliderCopie
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Formato
+                  Text("Formato"),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Radio(
+                          value: Formato.radio_12_14,
+                          groupValue: formato,
+                          onChanged: onRadioFormato,
+                        ),
+                        Text("12/14 pollici"),
+                        Radio(
+                          value: Formato.radio_15,
+                          groupValue: formato,
+                          onChanged: onRadioFormato,
+                        ),
+                        Text("15 pollici"),
+                        Radio(
+                          value: Formato.radio_altro,
+                          groupValue: formato,
+                          onChanged: onRadioFormato,
+                        ),
+                        Text("altro"),
+                      ]),
+                  // Copie
+                  Text("Copie: $copie"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Slider(
+                          value: copie.toDouble(),
+                          min: 1,
+                          max: 20,
+                          divisions: 20,
+                          label: copie.round().toString(),
+                          onChanged: onSliderCopie)
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
